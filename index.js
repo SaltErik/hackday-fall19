310c87b92de8b5d00b16bebc0266c44fa7ab71d4
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-01 00:56:36 +0100

    major refactor and fixes

--- index.js
+++ index.js
@@ -1,110 +1,65 @@
-'use strict';
-
-process.on('unhandledRejection', (error, promise) => {
-  console.log(' Oh Lord! We forgot to handle a promise rejection here: ', promise);
-  console.log(' The error was: ', error );
-});
-
-const wait = async () => {
-  return await setTimeout(console.log, 100000000)
-};
-
-const pause = async () => {
-  return await setTimeout(console.log, 1000)
-};
-
-// User facing API
-const { Android } = require('./android.js');
-
-const android = new Android();
-
-const demoStart = async () => {
-  console.log('Node started!');
-};
-
-const demoLs = async () => {
-  console.log('Calling android.ls()...');
-  console.log(await android.ls());
-  console.log('Done!');
-};
-
-const demoRm = async () => {
-  console.log('Calling android.rm()...');
-  console.log(await android.rm());
-  console.log('Done!');
-};
-
-const demoVibrate = async () => {
-  console.log('Calling android.vibrate()...');
-  console.log(await android.vibrate());
-  console.log('Done!');
-};
-
-const demoCameraInfo = async () => {
-  console.log('Calling android.cameraInfo()...');
-  console.log(await android.cameraInfo());
-  console.log('Done!');
-};
-
-const demoSetUpStorage = async () => {
-  console.log('Calling android.setUpStorage()...');
-  console.log(await android.setUpStorage());
-  console.log('Done!');
-};
-
-const demoCameraPhoto = async () => {
-  console.log('Calling android.cameraPhoto()...');
-  console.log(await android.cameraPhoto());
-  console.log('Done!');
-};
-
-const demoOpenFile = async () => {
-  console.log('Calling android.openFile()...');
-  console.log(await android.openFile());
-  console.log('Done!');
-};
-
-const demoOpenURL = async () => {
-  console.log('Calling android.openURL(`https://www.study-at-salt.com`)...');
-  console.log(await android.openURL(`https://www.study-at-salt.com`));
-  console.log('Done!');
-};
-
-const demoDialogConfirm = async () => {
-  console.log('Calling android.dialogConfirm()...');
-  console.log(await android.dialogConfirm());
-  console.log('Done!');
-};
-
-const demoEnd = async () => {
-  console.log('CTRL+C to kill node...');
-};
+`use strict`;
+const { promisify } = require(`util`);
+const run = promisify(require(`child_process`).execFile);
 
 
-async function run() {
-  const androidFunctions = [
-    demoStart,
-    demoLs,
-    pause,
-    demoRm,
-    pause,
-    demoLs,
-    pause,
-    // demoVibrate,
-    // demoCameraInfo,
-    // demoSetUpStorage,
-    demoCameraPhoto,
-    pause,
-    demoOpenFile,
-    pause,
-    // demoOpenURL,
-    // demoDialogConfirm,
-    wait,
-    demoEnd,
-  ];
-  for (const each of androidFunctions) {
-    await each(); // call function to get returned Promise
-  };
+class Android {
+
+  constructor() {
+    if (!!Android.instance) return Android.instance;
+    else Android.instance = this;
+  }
+
+  async getCameraInfo() {
+    return await JSON.parse(await run(`termux-camera-info`));
+  }
+
+  async takePhoto(saveAsName) {
+    const args = [];
+    if (saveAsName) args.push(`${saveAsName}.jpg`);
+    else args.push(`${Date.now()}.jpg`);
+    return await run(`termux-camera-photo`, args);
+  }
+
+  async ls() {
+    return await run(`ls`);
+  }
+
+  async rm(pathToFile) {
+    const args = [`-f`, `${pathToFile}`];
+    return await run(`rm`, args);
+  }
+
+  async vibrate(duration=1000, force=false) {
+    const args = [`-d`, `${duration}`];
+    if (force) args.push(`-f`);
+    return await run(`termux-vibrate`, args);
+  }
+
+  async setUpStorage() {
+    return await run(`termux-setup-storage`);
+  }
+
+  async openFile(pathToFile) {
+    const args = [`${pathToFile}`];
+    return await run(`termux-open`, args);
+  }
+
+  async openURL(URL) {
+    const args = [`${URL}`];
+    return await run(`termux-open`, args);
+  }
+
+  async showConfirmDialog(title=`Title goes here`, hint=`Hint goes here`) {
+    // -2: user clicks away
+    // -1: answers yes?
+    const args = [];
+    if (title) args.push(`-t`, `${title}`);
+    if (hint) args.push(`-i`, `${hint}`);
+    return await run(`termux-dialog`, args);
+  }
+
 }
 
-run();
+
+module.exports = { Android };
