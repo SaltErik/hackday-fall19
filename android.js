3e3d6fe17e43c1214728a1a3b0962b91c8bc19e3
SaltErik <erik.nygren@appliedtechnology.se>
2019-10-31 12:37:18 +0100

    hopefully better

--- android.js
+++ android.js
@@ -1,20 +1,24 @@
 'use strict';
 const { promisify } = require('util');
 const execFile = promisify(require('child_process').execFile);
+const apiPath = '/data/data/com.termux/files/usr/libexec/termux-api';
 
+const call = (apiCall) => apiPath.concat(apiCall);
 
 class Android {
 
   constructor() {
+    // Singleton pattern
     if (!!Android.instance) return Android.instance;
     else Android.instance = this;
   }
 
-  async vibrate(duration=1000, force=false) {
-    const args = ['-d', `${duration}`];
-    if (force === true) args.push('-f');
+  async vibrate(duration=1000, force=true) {
+    const args = []
+      .push(`--ei duration_ms ${duration}`)
+      .push(`--ez force ${force}`);
     try {
-      return await execFile('termux-vibrate', args);
+      return await execFile(call('Vibrate'), args);
     } catch (error) {
       console.log(`UH-OH! Something broke: ${error}`);
     }
